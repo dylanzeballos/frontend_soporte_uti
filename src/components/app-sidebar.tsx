e516583f9dd5eec08818ad80f407938d557656fa
Emmanuel Mamani <emmanuelmamani100@gmail.com>
2026-06-12 11:01:08 -0400

    update

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -27,7 +27,6 @@ import { cn } from "@/lib/utils"
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
--- src/hooks/useSidebarCounts.ts
+++ src/hooks/useSidebarCounts.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { fetchApi } from '@/hooks/useApi';
+
 
 export interface SidebarCounts {
   unassigned: number;
